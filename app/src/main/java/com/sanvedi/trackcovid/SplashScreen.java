9252004532b35be5663e7f69af62da54e3434952
aankit09 <aankitsanvedi@gmail.com>
2021-08-30 22:44:47 +0530

    update code

---
+++ app/src/main/java/com/sanvedi/trackcovid/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.sanvedi.trackcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 2500);
+    }
+}
